b7ece28ebfd79a37bef2800252a29aeb98b2c319
Thomas Mutzl <thomas@mutzl.com>
2021-04-29 02:45:41 +0200

    refactoring, adding another dataprovider

---
+++ Epg/DataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Mutzl.Homeassistant
+{
+    public class DataProvider
+    {
+        public string? Fullname { get; set; }
+        public IEnumerable<string>? Stations { get; set; }
+    }
+}
--- Epg/Show.cs
+++ Epg/Show.cs
@@ -4,11 +4,22 @@ namespace Mutzl.Homeassistant
 {
     public class Show
     {
-        public int? Id { get; set; }
-        public string? Title { get; set; }
+        public Show(string title, DateTime start, string station)
+        {
+            Title = title;
+            Start = start;
+            Station = station;
+        }
+
+        public string? Id { get; set; }
+        public string Title { get; set; }
+        public string? Episode { get; set; }
         public DateTime Start { get; set; }
+        public string Station { get; }
         public DateTime? End { get; set; }
         public string? Category { get; set; }
+
+
         public int? DurationInMinutes
         {
             get
@@ -27,9 +38,16 @@ namespace Mutzl.Homeassistant
                 if (totalDuration == null) return null;
 
                 var now = DateTime.Now;
+                if (Start > now) return 0.0;
+
                 var duration = (now - Start).TotalMinutes;
                 return duration / totalDuration;
             }
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Start:HH:mm}";
         }
     }
 }
